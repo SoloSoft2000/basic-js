0bcfc0742efeb3422b8210c3db2731e4b2c698ae
Eugene Solomonyk <solosoft2000@mail.ru>
2023-04-13 13:30:53 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let result = '';
+  if(members) {
+  for(let i=0; i<members.length; i++) {
+    if(typeof(members[i])==='string') {
+      result += members[i].trim().toUpperCase().slice(0,1)
+    }
+  };}
+  return result.length ? result.split('').sort().join('') : false;
 }
 
 module.exports = {
